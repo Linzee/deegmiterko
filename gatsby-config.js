746b6da69a7de05da74eba35bfb9d5f7dab5fd91
Linzee <d.gmiterko@gmail.com>
2021-11-06 12:32:39 +0100

    Add fb pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-facebook-pixel`,
       options: {
-        pixelId: "TODO pixel id here",
+        pixelId: "928689911405331",
       },
     },
     {
